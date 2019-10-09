4ccfbe50a388e70c6c4bb94a25124fc62db89e1c
Krishna Sannasi <krishna.sd.2012@gmail.com>
2019-10-09 16:19:28 -0700

    Local arena (#6)
    * added LocalArena
    changed Arena to use ShardedLock
    added Allocator api
    
    * added benchmarks
    
    * updated .gitignore
    
    * cargo fmt
    update main

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     use lexer_ext::token::Lexer;
 
-    let alloc = arena::Arena::new(10);
+    let alloc = arena::Arena::builder().build();
 
     let file = std::env::args().nth(1).expect("Please provide a file name");
     let file = std::fs::read_to_string(file).expect("Could not read file");
@@ -13,6 +13,17 @@ fn main() {
     let mut lexer = lexer::LexerImpl::new(&file);
     let mut parser = parser::ParserImpl::<&mut dyn Lexer<Input = _>>::new(&mut lexer);
 
-    println!("{}", parser.parse_expr(&alloc).unwrap());
-    // println!("{}\n------------------------------------------\n{0:#?}", parser.parse_expr(&alloc).unwrap());
+    let mut parse_iter = parser.parse(&alloc);
+
+    for ast in parse_iter.iter() {
+        println!("{}", ast);
+    }
+
+    println!("ERR = {:?}", parse_iter.err().unwrap());
+
+    // println!("------------------------------------------");
+
+    // for ast in ast {
+    //     println!("{:?}", ast);
+    // }
 }
